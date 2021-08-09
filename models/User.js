d30631941bc031d9dd8cea3c564826858c2cb219
Chiwon <emphasize1@gmail.com>
2021-08-09 21:09:59 +0900

    Feat: set userScheme

--- models/User.js
+++ models/User.js
@@ -5,6 +5,17 @@ const mongoose = require("mongoose");
   TODO: Fill in the model specification
 
  */
-const userSchema = new mongoose.Schema({});
+const userSchema = new mongoose.Schema({
+  email: {
+    type: String,
+    unique: 1,
+  },
+  password: {
+    type: String,
+    minlength: 5,
+  },
+  token: String,
+  tokenExp: Number,
+});
 
 module.exports = mongoose.model("User", userSchema);
