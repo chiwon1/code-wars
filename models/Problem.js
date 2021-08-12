92b25dfdeef5682b741690acc1557707e4ac6f1b
Chiwon <emphasize1@gmail.com>
2021-08-12 15:15:24 +0900

    Add: problemSchema
     - models/Problem.js

--- models/Problem.js
+++ models/Problem.js
@@ -1,10 +1,38 @@
 const mongoose = require("mongoose");
 
-/*
+const problemSchema = new mongoose.Schema({
+  id: {
+    type: Number,
+    required: true,
+  },
+  title: {
+    type: String,
+    required: true,
+  },
+  "completed_users": {
+    type: Number,
+    required: false,
+    default: 0,
+  },
+  "difficulty_level": {
+    type: Number,
+    required: false,
+    default: 1,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  tests: [{
+    code: {
+      type: String,
+      required: true,
+    },
+    solution: {
+      type: mongoose.Schema.Types.Mixed,
+      required: true,
+    },
+  }],
+});
 
-  TODO: Fill in the model specification
-
- */
-const ProblemSchema = new mongoose.Schema({});
-
-module.exports = mongoose.model("Problem", ProblemSchema);
+module.exports = mongoose.model("Problem", problemSchema);
