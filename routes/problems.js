f4a7aa324dbe438980fe8b22cc9a29ed39464ac9
Chiwon <emphasize1@gmail.com>
2021-08-12 19:09:40 +0900

    Feat: enable compile and run input solution code
     - use vm

--- routes/problems.js
+++ routes/problems.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const Problem = require("../models/Problem");
+const vm = require("vm");
 
 router.get("/:problem_id", async (req, res, next) => {
   const id = parseInt(req.params.problem_id);
@@ -14,6 +15,20 @@ router.post("/:problem_id", async (req, res, next) => {
   const problem = await Problem.findOne({ id });
   const testCases = problem.tests;
   const solution = req.body.solution;
+
+  const context = { result: null };
+  const failedTestCases = [];
+
+  for (let  i = 0; i < testCases.length; i++) {
+    const code = `${solution} \n result = ${testCases[i].code}`;
+
+    try {
+      vm.runInNewContext(code, context);
+    } catch (err) {
+      return;
+    }
+  }
+
 });
 
 module.exports = router;
