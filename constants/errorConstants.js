3b98e5827ef58c31a1d65402bb9ea4e48a23bcea
Chiwon <emphasize1@gmail.com>
2021-08-15 17:03:02 +0900

    Fix: add error handling for invalid sign up input from client
     - change /route/signup.js
     - add condition handling input white space for user name
     - add condition handling input email not following email form

--- constants/errorConstants.js
+++ constants/errorConstants.js
@@ -1,6 +1,7 @@
 const ERROR_NOT_FOUND = "404 Page Not Found";
 const ERROR_500_SERVER = "500 Server Error";
 const ERROR_INVALID_SIGNUP_INPUT = "Enter valid information";
+const ERROR_DUPLICATE_EMAIL = "Entered ID already exists.";
 const ERROR_INVALID_PROBLEM_ID = "Invalid problem request";
 const ERROR_INVALID_SOLUTION_INPUT = "Invalid solution input";
 
@@ -8,6 +9,7 @@ module.exports = {
   ERROR_NOT_FOUND,
   ERROR_500_SERVER,
   ERROR_INVALID_SIGNUP_INPUT,
+  ERROR_DUPLICATE_EMAIL,
   ERROR_INVALID_PROBLEM_ID,
   ERROR_INVALID_SOLUTION_INPUT,
 };
--- routes/signup.js
+++ routes/signup.js
@@ -1,28 +1,46 @@
 const express = require("express");
 const router = express.Router();
 
+const validator = require("email-validator");
+
 const User = require("../models/User");
-const { ERROR_INVALID_SIGNUP_INPUT } = require("../constants/errorConstants");
+const { ERROR_INVALID_SIGNUP_INPUT, ERROR_DUPLICATE_EMAIL } = require("../constants/errorConstants");
 
 router.get("/", function (req, res, next) {
   res.render("signup", { title: "바닐라코딩" });
 });
 
 router.post("/", function (req, res, next) {
-  const { name, email, password } = req.body;
-
-  if (!name || !email || !password) {
-    next(createError(400, ERROR_INVALID_SIGNUP_INPUT));
-  }
-
-  const user = new User(req.body);
-
   try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password) {
+      throw createError(400, ERROR_INVALID_SIGNUP_INPUT);
+    }
+
+    if (!name.trim()) {
+      throw createError(400, ERROR_INVALID_SIGNUP_INPUT);
+    }
+
+    if (!validator.validate(email)) {
+      throw createError(400, ERROR_INVALID_SIGNUP_INPUT);
+    }
+
+    const user = new User(req.body);
+
     user.save();
 
     return res.redirect(302, "/login");
   } catch (err) {
-    return next(err);
+    if (err.name === "MongoError" && err.code === 11000) {
+      return next(createError(400, ERROR_DUPLICATE_EMAIL));
+    }
+
+    if (err instanceof mongoose.Error.ValidationError) {
+      return next(createError(400, ERROR_INVALID_SIGNUP_INPUT));
+    }
+
+    next(err);
   }
 });
 
